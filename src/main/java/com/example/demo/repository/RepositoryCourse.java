c1b7bf46982f014621d57e67b4b1b2248cd21789
saifoelloh <saifoelloh@gmail.com>
2019-08-17 01:10:46 +0700

    add dependency injection to course repository

--- src/main/java/com/example/demo/repository/RepositoryCourse.java
+++ src/main/java/com/example/demo/repository/RepositoryCourse.java
@@ -2,6 +2,8 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Course;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RepositoryCourse extends CrudRepository<Course, Long> {
 }
