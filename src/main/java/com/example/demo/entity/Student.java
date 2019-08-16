56610e76712bcab2cca0380510607fead668d435
saifoelloh <saifoelloh@gmail.com>
2019-08-17 01:08:48 +0700

    implementing lombok to student entity

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,32 +1,32 @@
 package com.example.demo.entity;
 
 import lombok.Data;
-import lombok.Generated;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.LocalDateTime;
 
+@NoArgsConstructor
 @Data
-
 @Entity
 public class Student {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue
     private Long id;
     private String name;
-
-    @UpdateTimestamp
-    private LocalDateTime updated_at;
 
     @CreationTimestamp
     private LocalDateTime created_at;
 
+    @UpdateTimestamp
+    private LocalDateTime updated_at;
+
     public Student(String name) {
         this.name = name;
     }
+
 }
