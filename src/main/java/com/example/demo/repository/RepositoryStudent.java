da5cc08115b96d6897af2d865f6e472a87eb57d1
saifoelloh <saifoelloh@gmail.com>
2019-08-17 01:11:01 +0700

    add dependency injection to student repository

--- src/main/java/com/example/demo/repository/RepositoryStudent.java
+++ src/main/java/com/example/demo/repository/RepositoryStudent.java
@@ -2,6 +2,8 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Student;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RepositoryStudent extends CrudRepository<Student, Long> {
 }
