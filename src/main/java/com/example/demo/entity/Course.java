edb0ea2b3014894cb1525a8e161516ff89868a4a
saifoelloh <saifoelloh@gmail.com>
2019-08-17 01:08:32 +0700

    implementing lombok to course entity

--- src/main/java/com/example/demo/entity/Course.java
+++ src/main/java/com/example/demo/entity/Course.java
@@ -1,20 +1,30 @@
 package com.example.demo.entity;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedEntityGraph;
+import java.time.LocalDateTime;
 
+@NoArgsConstructor
 @Data
-
 @Entity
 public class Course {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue
     private Long id;
     private String name;
+
+    @CreationTimestamp
+    private LocalDateTime created_at;
+
+    @UpdateTimestamp
+    private LocalDateTime updated_at;
 
     public Course(String name) {
         this.name = name;
