d112e0457bc2f9c2cd37f0351b5b733cd767048c
saifoelloh <saifoelloh@gmail.com>
2019-08-17 01:11:39 +0700

    add dependency injection to student service

--- src/main/java/com/example/demo/service/ServiceStudent.java
+++ src/main/java/com/example/demo/service/ServiceStudent.java
@@ -4,10 +4,12 @@ import com.example.demo.entity.Student;
 import com.example.demo.repository.RepositoryStudent;
 import com.example.demo.service.impl.InterfaceStudent;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class ServiceStudent implements InterfaceStudent {
     @Autowired private RepositoryStudent repository;
 
